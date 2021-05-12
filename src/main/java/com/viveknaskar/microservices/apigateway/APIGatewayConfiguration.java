28e0f95471056ce6c21badabd511a4bdd853bf7a
Vivek Naskar <viveknaskar2912@gmail.com>
2021-05-13 03:07:01 +0530

    Added routing in API gateway

---
+++ src/main/java/com/viveknaskar/microservices/apigateway/APIGatewayConfiguration.java
@@ -0,0 +1,32 @@
+package com.viveknaskar.microservices.apigateway;
+
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.Buildable;
+import org.springframework.cloud.gateway.route.builder.PredicateSpec;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.function.Function;
+
+@Configuration
+public class APIGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p.path("/get")
+                .filters(f -> f
+                        .addRequestHeader("Header", "MyURI")
+                        .addRequestParameter("Parameters", "MyParams"))
+                .uri("http://httpbin.org:80"))
+                .route(p -> p.path("/currency-exchange/**")
+                        .uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion/**")
+                        .uri("lb://currency-conversion"))
+                .route(p -> p.path("/currency-conversion-feign/**")
+                        .uri("lb://currency-conversion"))
+                .build();
+    }
+}
