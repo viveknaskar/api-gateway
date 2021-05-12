2aeb0ce0c8f3865f526ab32137cd99003e278663
Vivek Naskar <viveknaskar2912@gmail.com>
2021-05-13 03:07:38 +0530

    Removed unused imports

--- src/main/java/com/viveknaskar/microservices/apigateway/APIGatewayConfiguration.java
+++ src/main/java/com/viveknaskar/microservices/apigateway/APIGatewayConfiguration.java
@@ -1,14 +1,9 @@
 package com.viveknaskar.microservices.apigateway;
 
-import org.springframework.cloud.gateway.route.Route;
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.function.Function;
 
 @Configuration
 public class APIGatewayConfiguration {
